5a143e8a6f636ab58d8c74dab244716a4428dc4e
Shaws <seanrichardparker@gmail.com>
2018-05-12 06:53:58 -0400

    current_user concern layout of OpenStruct

--- app/controllers/concerns/current_user.rb
+++ app/controllers/concerns/current_user.rb
@@ -6,6 +6,14 @@ module CurrentUser
   end
 
   def current_user
-    super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+    super || guest_user
+  end
+
+  def guest_user
+    OpenStruct.new(name: "Guest User", 
+                  first_name: "Guest", 
+                  last_name: "User", 
+                  email: "guest@example.com"
+                  )
   end
 end
